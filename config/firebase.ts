41cfe27d1b55e129145f0c5544c9d8d7596e4d74
sarzeez <sarzeez1997@gmail.com>
2022-07-28 17:12:11 +0500

    setting up new project with firebase

---
+++ config/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcoXRs5fQBSjRimg0CSzvb_7IbbsMeApY",
+  authDomain: "nextjs-crud-cca8f.firebaseapp.com",
+  projectId: "nextjs-crud-cca8f",
+  storageBucket: "nextjs-crud-cca8f.appspot.com",
+  messagingSenderId: "827924922932",
+  appId: "1:827924922932:web:54b7036b75cb3059a0c061"
+};
+
+const app = initializeApp(firebaseConfig);
